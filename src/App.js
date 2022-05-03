4611540e434781d810609e18c3c155529c1d3289
shoronshaha <smaranshaha@gmail.com>
2022-05-03 15:51:23 +0600

    create register page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home/Home';
 import Login from './Components/Login/Login/Login';
-// import Register from './Components/Login/Register/Register';
+import Register from './Components/Login/Register/Register';
 import Footer from './Components/Shared/Footer/Footer';
 import Navbar from './Components/Shared/Navbar/Navbar';
 
@@ -16,7 +16,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
-        {/* <Route path='/register' element={<Register />} /> */}
+        <Route path='/register' element={<Register />} />
 
       </Routes>
       <Footer></Footer>
