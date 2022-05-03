5ff2c861dc3f3ca8b91971488e978f0193d58ac0
shoronshaha <smaranshaha@gmail.com>
2022-05-03 15:01:54 +0600

    create a login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home/Home';
+import Login from './Components/Login/Login/Login';
+// import Register from './Components/Login/Register/Register';
 import Footer from './Components/Shared/Footer/Footer';
 import Navbar from './Components/Shared/Navbar/Navbar';
 
@@ -13,6 +15,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        {/* <Route path='/register' element={<Register />} /> */}
+
       </Routes>
       <Footer></Footer>
     </div>
